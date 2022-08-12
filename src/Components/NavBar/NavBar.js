2c82e98e657fba783b4eacd9ae320fc3b4c77955
okhot <arreyetta1jnr@gmail.com>
2022-08-12 19:34:40 +0100

    navbar created

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../NavBar/NavBar.css'
+
 
 const NavBar = () => {
   return (
-    <div>
-      <h1>Hello Worlds</h1>
+     <section>
+           <div className="navbar">
+            <div className="navbar__links">
+      <Link to='/' className="navbar__link">Music</Link>
+      <Link to='/' className="navbar__link">Podcast</Link>
+      <Link to='/' className="navbar__link">Radio</Link>
+      <Link to='/' className="navbar__link">Live</Link>
+            </div>
+            <input type="text" placeholder="Search..." className="navbar__search" />
+            <span class="material-symbols-outlined">
+                favorite
+            </span>
+
+            <span className="material-symbols-outlined">
+                notifications
+            </span>
+     
+            <span className="material-symbols-outlined">
+                account_circle
+            </span>
+            <h4>Ryan Ahzari</h4>
+      
     </div>
+  
+     </section>
   )
 }
 
