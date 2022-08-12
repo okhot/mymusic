970cf207d6ea115bc8049aeb3e434dc79ab6d151
Kongnyu Roger <kongnyuroger711@gmail.com>
2022-08-13 00:30:16 +0100

    getting done with left side bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@
 import './App.css';
 import { BrowserRouter as Router, Routers, Routes } from 'react-router-dom';
 import NavBar from './Components/NavBar/NavBar';
+import SideBar from './Components/sidebar/SideBar';
 
 function App() {
   return (
    <Router>
-    <NavBar />
+    <SideBar/>
    </Router>
   );
 }
