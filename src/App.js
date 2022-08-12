35f1d4a59d2a7cb94433466ba2dc41f7411c1695
Kongnyu Roger <kongnyuroger711@gmail.com>
2022-08-12 17:05:25 +0100

    testing

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { BrowserRouter as Router, Routers, Routes } from 'react-router-dom';
+import NavBar from './Components/NavBar/NavBar';
+
+function App() {
+  return (
+   <Router>
+    <NavBar />
+   </Router>
+  );
+}
+
+export default App;
