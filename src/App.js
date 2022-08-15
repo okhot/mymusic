ebd5ab69a4d6b61d74d4133275fdef2cf79e1d49
Kongnyu Roger <kongnyuroger711@gmail.com>
2022-08-15 12:17:12 +0100

    oo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { BrowserRouter as Router, Routers, Routes } from 'react-router-dom';
 import SideBar from './Components/sidebar/SideBar';
 import Login from './Components/screens/auths/Login';
-
+import NavBar from './Components/NavBar/NavBar';
 function App() {
   return (
    <Router>
+    <NavBar />
     <Login />
     <SideBar />
+
    </Router>
   );
 }
